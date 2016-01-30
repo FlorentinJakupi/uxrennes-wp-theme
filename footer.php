f9ed78f625b60c9bee840b14e352a50cb7711121
Marie Guillaumet <marie@marieguillaumet.com>
2016-01-30 11:04:56 +0100

    Modif. footer

--- footer.php
+++ footer.php
@@ -16,8 +16,6 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
 					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'uxrennes-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'uxrennes-theme' ), 'WordPress' ); ?></a>
-					<span class="sep"> | </span>
-					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'uxrennes-theme' ), 'uxrennes-theme', '<a href="http://residence-mixte.com" rel="designer">Résidence Mixte</a>' ); ?>
 				</div>
 			</footer>
 		</div>
